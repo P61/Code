31c1358126a597445cbdb553c27f015f73a5b75e
P61 <18339474940@163.com>
2019-12-10 22:33:54 +0800

    更新

--- 07.Selenium库.py
+++ 07.Selenium库.py
@@ -241,10 +241,69 @@ print(input, button)
 #更多等待的条件：https://selenium-python.readthedocs.io/waits.html
 # https://selenium-python.readthedocs.io/api.html#module-selenium.webdriver.support.expected_conditions
 # =============================================================================
-# 
-    
+# 前进后退
+from selenium import webdriver
+import time
+
+browser = webdriver.Chrome()
+browser.get('https://www.baidu.com')
+browser.get('https://www.python.org')
+browser.get('https://www.taobao.com')
+browser.back()
+time.sleep(1)
+browser.forward()
+browser.close()
+
+
 # =============================================================================
-# 
+# Cookies
+from selenium import webdriver
 
-    
+browser = webdriver.Chrome()
+browser.get('https://www.zhihu.com/explore')
+print(browser.get_cookies())
+browser.add_cookie({'name':'name', 'domain':'www.zhihu.com', 'value':'germey'})
+print(browser.get_cookies())
+browser.delete_all_cookies()
+print(browser.get_cookies())
 
+# =============================================================================
+# 选项卡管理
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+browser.get('https://www.baidu.com')
+browser.execute_script('window.open()')
+print(browser.window_handles)
+browser.switch_to_window(browser.window_handles[1])
+browser.get('https://www.taobao.com')
+time.sleep(1)
+browser.switch_to_window(browser.window_handles[0])
+browser.get('https://www.python.org')
+# 通过执行js打开一个新的选项卡，选项卡之间的切换
+
+# =============================================================================
+# 异常处理
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+browser.get('https://www.baidu.com')
+browser.find_element_by_id('hello')
+
+
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException,NoSuchElementException
+
+browser = webdriver.Chrome()
+try:
+    browser.get('https://www.baidu.com')
+except TimeoutException:
+    print("Time Out")
+try:
+    browser.find_element_by_id('hello')
+except NoSuchElementException:
+    print("No Element")
+finally:
+    browser.close()
+
+# 更多异常捕获 https://selenium-python.readthedocs.io/api.html#module-selenium.common.exceptions
