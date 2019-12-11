4cf678797d07c446ce56a7e1001ab3caa3d9e50d
P61 <18339474940@163.com>
2019-12-11 21:23:13 +0800

    更新

--- 08,Requests+正则表达式爬取猫眼电影TOP100.py
+++ 08,Requests+正则表达式爬取猫眼电影TOP100.py
@@ -3,6 +3,8 @@
 import requests
 from requests.exceptions import RequestException
 import re
+import json
+from multiprocessing import Pool
 
 def get_one_page(url):
     '''
@@ -20,6 +22,7 @@ def get_one_page(url):
     except RequestException:
         return None
 
+
 def Re(string):
     '''
         用正则匹配出有用的信息
@@ -27,32 +30,53 @@ def Re(string):
 #    filmname = []
 #    temp1 = re.findall('<p class="name"><a.*?>(.*?)</a></p>',string,re.S)
 #    filmname.append(temp1)
-    filmname = re.findall('<p class="name"><a.*?>(.*?)</a></p>',string,re.S)
+    index = re.findall('<i class="board-index board-index-.*?">(.*?)</i>',string,re.S)
+    img = re.findall('<img data-src="(.*?)"',string,re.S)
+    title = re.findall('<p class="name"><a.*?>(.*?)</a></p>',string,re.S)
     star = re.findall('<p class="star">\n.*?主演：(.*?)\n.*?</p>',string,re.S)
     releasetime = re.findall('<p class="releasetime">上映时间：(.*?)</p>',string,re.S)
-    print(filmname)
-    print(star)
-    print(releasetime)
+    score = re.findall('<i class="integer">(.*?)</i><i class="fraction">(.*?)</i></p>',string,re.S)
+#    print(index)
+#    print(img)
+#    print(title)
+#    print(star)
+#    print(releasetime)
+#    print(score)
+    for k in range(10):
+        yield {
+            'index': index[k],
+            'title': title[k],
+            'star': star[k],
+            'releasetime': releasetime[k],
+            'score': score[k][0]+score[k][1],
+            'image': img[k]
+        }
+    
+#    print(scores[0]|scores[1])
 # <li.*?>\s*?(<a.*?>)?(\w+)(</a>)?\s*?</li>
 
 def Write(string):
     '''
         写入本地
     '''
-    with open('HTML.html', 'w', encoding="utf8") as HTML:
-        HTML.write(string)
+    with open('result.txt', 'a', encoding="utf-8") as f:
+        f.write(json.dumps(string, ensure_ascii=False) + '\n')
+        f.close()
 
 
-def main():
-    url = 'https://maoyan.com/board/4'
+def main(offset):
+    
+    url = 'https://maoyan.com/board/4?offset=' + str(offset)
     html = get_one_page(url)
-    Re(html)
-#    print(html)
-#    Write(html)
+    for item in Re(html):
+        print(item)
+        Write(item)
 
 if __name__=='__main__':
-    main()
+    for k in range(10):
+        main(k*10)
+#    pool = Pool()
+#    pool.map(main, [i*10 for i in range(10)])
+#    使用多线程会出现同时写入问题，顺序会不一样，并且同一个项目会被切开
 
-    
-    
     
